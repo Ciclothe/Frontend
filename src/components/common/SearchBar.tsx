a66d47ee9f9342edd77bf8aa2f6a6a75c4142e8d
Alejando Ospina Rojas <aosproj@upv.edu.es>
2024-11-11 17:24:17 +0100

    CC-front-1-Redesign-Header-Component

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useTheme } from "@/context/ThemeContext";
+import Icon from "@mdi/react";
+import { mdiMagnify, mdiCrosshairsGps } from "@mdi/js";
+import { useTranslation } from "react-i18next";
+
+const SearchBar: React.FC = () => {
+  const { isNightMode } = useTheme();
+  const { t } = useTranslation();
+
+  return (
+    <div className="w-full flex items-center justify-center">
+      <div
+        className={`${
+          isNightMode ? "bg-[#232323]" : "bg-white"
+        } rounded-full w-full flex items-center justify-between`}
+      >
+        {/* ELEMENT SEARCH */}
+        <div className="px-3 flex items-center w-full">
+          <div
+            onClick={() => {
+              const inputElement = document.getElementById(
+                "searchInput"
+              ) as HTMLInputElement | null;
+              if (inputElement) {
+                inputElement.focus();
+              }
+            }}
+          >
+            {" "}
+            <Icon
+              path={mdiMagnify}
+              size={1}
+              className="cursor-pointer opacity-50"
+            />
+          </div>
+          <input
+            id="searchInput"
+            type="text"
+            placeholder={t("Global.SearchHolder")}
+            className="bg-transparent w-full outline-none rounded-full text-white p-2"
+          />
+        </div>
+        {/* LOCATION SEARCH */}
+        <div className="bg-[#02995D] border text-[#02995D] border-[#02995D] bg-opacity-10 border-2 h-full gap-6 p-2 md:aspect-square lg:aspect-auto px-4 md:px-[0px] py-[2px] md:py-[0px] lg:py-[2px] rounded-full lg:px-4 flex items-center">
+          <div className="flex md:hidden lg:flex flex-col whitespace-nowrap">
+            <h2 className="font-bold">Valencia, Spn.</h2>
+            <p className="mt-[-3px]">2 km</p>
+          </div>
+          <div className={`${isNightMode ? "text-white" : "text-[#232323]"}`}>
+            <Icon path={mdiCrosshairsGps} size={0.7} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -17,10 +17,10 @@ export const MainLayout: FC = () => {
   return (
     <>
       {/* Desktop View */}
-      <div className="px-[2em] pt-[3em] hidden text-black lg:grid grid-cols-12">
+      <div className="px-[1em] lg:px-[5em] text-black lg:grid grid-cols-12">
         {/* Header */}
-        <header className="h-[6em] col-span-12">
-          <Header onMenuClick={handleMenuClick} />
+        <header className="col-span-12">
+          <Header />
         </header>
 
         <div className="col-span-12 flex">
@@ -37,37 +37,6 @@ export const MainLayout: FC = () => {
 
           {/* Right Sidebar */}
           <div className="lg:min-w-[25vw] lg:max-w-[25vw] xl:min-w-[20vw] xl:max-w-[20vw] relative">
-            <SidebarRight />
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile View */}
-      <div className="block lg:hidden text-black">
-        {/* Header */}
-        <header>
-          <Header onMenuClick={handleMenuClick} />
-        </header>
-
-        {showLeftMenu && (
-          <ClickAwayListener onClickAway={handleMenuClick}>
-            <div
-              className="fixed h-full hidden sm:block top-0 h-[100vh]"
-              style={{ zIndex: 100 }}
-            >
-              <SidebarLeftMobile />
-            </div>
-          </ClickAwayListener>
-        )}
-
-        <div className="flex">
-          {/* Main Content */}
-          <div className="p-5 lg:p-4 flex-grow">
-            <Outlet />
-          </div>
-
-          {/* Right Sidebar */}
-          <div className="md:min-w-[35vw] md:max-w-[35vw] hidden md:block relative">
             <SidebarRight />
           </div>
         </div>
