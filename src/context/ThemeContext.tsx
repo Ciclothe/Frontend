3fbf0b47b07bbffe522564efb66d0662916fa5e8
Alejando Ospina Rojas <aosproj@upv.edu.es>
2024-11-17 17:18:30 +0100

    fixing feed view

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useState, useContext, ReactNode } from "react";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
 
 interface ThemeContextProps {
   isNightMode: boolean;
@@ -10,7 +16,36 @@ const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 export const ThemeProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [isNightMode, setIsNightMode] = useState(true);
+  const [isNightMode, setIsNightMode] = useState<boolean>(false);
+
+  // Detectar el modo de preferencia del navegador (claro/oscuro)
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setIsNightMode(mediaQuery.matches);
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setIsNightMode(e.matches); // Actualiza el estado cuando cambie la preferencia
+    };
+
+    // Escuchar los cambios en la preferencia del esquema de color
+    mediaQuery.addEventListener("change", handleChange);
+
+    // Limpiar el event listener cuando el componente se desmonte
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  // Aplicar la clase correspondiente al body dependiendo del tema
+  useEffect(() => {
+    if (isNightMode) {
+      document.body.classList.add("bodyNightMode");
+      document.body.classList.remove("bodyDayMode");
+    } else {
+      document.body.classList.add("bodyDayMode");
+      document.body.classList.remove("bodyNightMode");
+    }
+  }, [isNightMode]);
 
   const toggleMode = () => {
     setIsNightMode((prev) => !prev);
