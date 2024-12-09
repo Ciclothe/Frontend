eb9aa3cd450d45de9abdfeb3f1e449ba9356bd5b
mariapas-ai <mariapastormo2006@gmail.com>
2024-12-09 13:06:54 +0100

    40 Menu design

--- src/components/ProfileDropdown.tsx
+++ src/components/ProfileDropdown.tsx
@@ -7,13 +7,13 @@ const ProfileDropdown: React.FC = () => {
   return (
     <div className="relative inline-block">
       <div
-        className="w-10 h-10 cursor-pointer rounded-full overflow-hidden"
+        className="w-10 h-10 cursor-pointer rounded-full"
         onClick={toggleDropdown}
       >
         <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fes%2Ffotos%2Ffoto-de-perfil&psig=AOvVaw0P4fHZTvs7c4UIlUxrwqm5&ust=1733261238429000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKiburaDiooDFQAAAAAdAAAAABAE" />
       </div>
       {isOpen && (
-        <div className="absolute right-0 top-12 bg-white border border-gray-300 rounded-lg shadow-lg w-40 py-2">
+        <div className="absolute right-0 top-12 bg-white border-gray-300 rounded-lg shadow-lg w-40 py-2">
           <ul>
             <li>
               <a
