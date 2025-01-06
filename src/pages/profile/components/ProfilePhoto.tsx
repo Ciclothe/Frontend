c9f27cb9d3c88b2c7191c146cd826878fb7f0589
Alejando Ospina Rojas <aosproj@upv.edu.es>
2025-01-06 19:08:02 +0100

    93-redesign-events-view

--- src/pages/profile/components/ProfilePhoto.tsx
+++ src/pages/profile/components/ProfilePhoto.tsx
@@ -1,4 +1,4 @@
-function ProfilePhoto({ profilePic }) {
+function ProfilePhoto({ profilePic }: any) {
   return (
     <div
       className="h-[15em] w-[15em] rounded-full bg-cover bg-center mt-[-14vh]"
